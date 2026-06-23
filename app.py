0722454158d78fce9bc13602dac40f2e5a4a6f8a
IndralekhaA <indralekhaalmala@gmail.com>
2026-06-23 19:40:10 -0400

    Task4 Completed: Added Add Job page and View Job page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from extensions import db
 from models.job import Job
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -14,6 +15,40 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/add-job", methods = ["GET", "POST"])
+def add_job():
+
+    if request.method == "POST":
+        company = request.form["company"]
+        role = request.form["role"]
+        status = request.form["status"]
+        date_str = request.form["date_applied"]
+        date_applied = datetime.strptime(date_str, "%Y-%m-%d").date()
+        notes = request.form["notes"]
+
+        new_job = Job(
+            company = company,
+            role = role,
+            status = status,
+            date_applied = date_applied,
+            notes = notes
+        )
+        print(date_applied)
+        print(type(date_applied))
+
+        db.session.add(new_job)
+        db.session.commit()
+
+        return redirect(url_for("home"))
+    return render_template("add_job.html")
+
+@app.route("/jobs")
+def view_jobs():
+
+    jobs = Job.query.all()
+
+    return render_template("jobs.html", jobs = jobs)
 
 if __name__ == "__main__":
     app.run(debug=True)
